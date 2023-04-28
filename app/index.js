1841e15a9877a47d3341d9b8a4c5ccb0d3f70a45
ahmadsalahuddeen <ahmadsalahuddeen6017@gmail.com>
2023-04-28 10:02:29 +0530

    final commit

--- app/index.js
+++ app/index.js
@@ -1,7 +1,15 @@
 const app = require('express')();
-
+const appid = process.env.APPID;
 
 app.get('/', (req, res ) => 
-res.send('hi from dockerrr container'))
+res.send(appid +'rooot hi from dockerrr container'))
+app.get('/product', (req, res ) => 
+res.send(appid +' product   hi from dockerrr container'))
+app.get('/cat', (req, res ) => 
+res.send(appid +'cattt  hi from dockerrr container'))
+app.get('/cat3', (req, res ) => 
+res.send(appid +'cat3333 hi from dockerrr container'))
+app.get('/user', (req, res ) => 
+res.send(appid +'user     hi from dockerrr container'))
 
-app.listen(9999, ()=> console.log('listening on http://localhost:9999'))
+app.listen(appid, ()=> console.log('listening on http://localhost:'+ appid))
